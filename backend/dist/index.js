782a7185435b36605e4346898a32ebcb4469f268
Riya <riya03.wall@gmail.com>
2024-01-20 23:23:11 -0800

    added json write

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,9 +1,32 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
 };
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
@@ -48,7 +71,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var class_validator_1 = require("class-validator");
 var cors_1 = __importDefault(require("cors"));
-var class_transformer_1 = require("class-transformer");
+var fs = __importStar(require("fs-extra"));
 var path = require('path');
 var app = (0, express_1.default)();
 var RequestBody = /** @class */ (function () {
@@ -74,27 +97,27 @@ var ResponseBody = /** @class */ (function () {
 // You might want to disbale this on production.
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
-app.post('/api', function (req, res) {
+// app.post('/api', async function(req: Request, res: Response) {
+//     let body = plainToClass(RequestBody, req.body as Object);
+//     let validationErrors = await validate(body);
+//     if (validationErrors.length == 0) {
+//         const responseBody: ResponseBody = new ResponseBody("Hello, " + body.name);
+//         res.contentType('application/json');
+//         res.status(200);
+//         res.send(responseBody);
+//     } else {
+//         res.sendStatus(400);
+//     }
+// });
+app.post("/enterData", function (req, res) {
     return __awaiter(this, void 0, void 0, function () {
-        var body, validationErrors, responseBody;
+        var parseData, stringify;
         return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    body = (0, class_transformer_1.plainToClass)(RequestBody, req.body);
-                    return [4 /*yield*/, (0, class_validator_1.validate)(body)];
-                case 1:
-                    validationErrors = _a.sent();
-                    if (validationErrors.length == 0) {
-                        responseBody = new ResponseBody("Hello, " + body.name);
-                        res.contentType('application/json');
-                        res.status(200);
-                        res.send(responseBody);
-                    }
-                    else {
-                        res.sendStatus(400);
-                    }
-                    return [2 /*return*/];
-            }
+            parseData = JSON.parse(req.body);
+            stringify = JSON.stringify(parseData);
+            fs.writeFileSync("./persistence/" + parseData.date, stringify);
+            res.send("hello");
+            return [2 /*return*/];
         });
     });
 });
--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,17 @@
-import express, {NextFunction, Request, RequestHandler, Response} from "express";
+import {NextFunction, Request, RequestHandler, Response} from "express";
 import {Length, IsString, Validate, validate} from "class-validator";
 import cors from "cors";
 import {plainToClass} from "class-transformer";
+import * as fs from 'fs-extra';
+
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const path = require('path');
 const app = express();
+
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
 
 class RequestBody {
     @Length(1, 30)
@@ -30,17 +37,24 @@ class ResponseBody {
 app.use(cors());
 app.use(express.json() as RequestHandler);
 
-app.post('/api', async function(req: Request, res: Response) {
-    let body = plainToClass(RequestBody, req.body as Object);
-    let validationErrors = await validate(body);
-    if (validationErrors.length == 0) {
-        const responseBody: ResponseBody = new ResponseBody("Hello, " + body.name);
-        res.contentType('application/json');
-        res.status(200);
-        res.send(responseBody);
-    } else {
-        res.sendStatus(400);
-    }
+// app.post('/api', async function(req: Request, res: Response) {
+//     let body = plainToClass(RequestBody, req.body as Object);
+//     let validationErrors = await validate(body);
+//     if (validationErrors.length == 0) {
+//         const responseBody: ResponseBody = new ResponseBody("Hello, " + body.name);
+//         res.contentType('application/json');
+//         res.status(200);
+//         res.send(responseBody);
+//     } else {
+//         res.sendStatus(400);
+//     }
+// });
+
+app.post("/enterData", async function(req: Request, res: Response) {
+    let parseData = JSON.parse(req.body);
+    let stringify = JSON.stringify(parseData);
+    fs.writeFileSync("./persistence/" + parseData.date, stringify)
+    res.send("hello");
 });
 
 app.use(express.static(path.join(__dirname, 'build')));
